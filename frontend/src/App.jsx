1b245ee0011ac4f9c495ac7975d0aa88ba3f968b
Inês Jobino <inesjobino336@gmail.com>
2026-03-30 17:19:29 +0100

    some commits

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+const Header = () => {
+    return(
+        <div className="head">
+            <h1>Dashboar</h1>
+            <p>A brief description about the project</p>
+        </div>
+    );
+}
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+
