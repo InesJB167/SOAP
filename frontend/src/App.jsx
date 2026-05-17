2123c5de0751a0246c35d454153bd03f97dc98e4
Inês Jobino <inesjobino336@gmail.com>
2026-05-17 15:41:09 +0100

    organizations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,4 @@
 
-
-const Header = () => {
-    return(
-        <div className="head">
-            <h1>Dashboar</h1>
-            <p>A brief description about the project</p>
-        </div>
-    );
-}
-
 export function App() {
 
   return (
--- frontend/src/header.jsx
+++
@@ -1,2 +0,0 @@
-//creating a react component
-
